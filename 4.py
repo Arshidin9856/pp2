5777cf1e447876dd948110f44051f447b5dfd4f8
Arshidin9856 <98646863+Arshidin9856@users.noreply.github.com>
2022-01-30 20:57:21 +0600

    lab1 changed

--- 4.py
+++ 4.py
@@ -1,21 +1,3 @@
 a=int(input())
 b=input()
 
-if b=='k':
-    c=a
-    c/=1024
-    if not(type(c)==int):
-       x=int(input())
-       print('%.xf' %c)
-    else:
-        print(int(c))
-else:
-    a=1024*a
-    if not(type(a)==int):
-       c=int(input())
-       print('%.int(c)f' %a)
-    else:
-        print(int(a))
-
-
-   
