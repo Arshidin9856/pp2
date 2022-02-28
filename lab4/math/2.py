bf9065a6c392aeb6a8795a0560b5189e7b89566f
Arshidin9856 <98646863+Arshidin9856@users.noreply.github.com>
2022-02-28 20:29:02 +0600

    lab4 added

---
+++ lab4/math/2.py
@@ -0,0 +1,11 @@
+import math
+'''
+Height: 5
+Base, first value: 5
+Base, second value: 6
+Expected Output: 27.5
+'''
+H=int(input('Height: '))
+b1=int(input('Base, first: '))
+b2=int(input('Base, second: '))
+print(f"Expected Output: {H/2*(b1+b2)}")
---
+++ lab4/math/3.py
@@ -0,0 +1,6 @@
+import math
+n=int(input())
+a=int(input())
+print(n*(a**2)/(math.tan(180/n)*4))
+
+print(math.tan(45))
